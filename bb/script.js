5b66e1ffe00474629c1c421e93e3f5892d626923
ashrittt <studujccampos@gmail.com>
2026-05-30 00:03:25 +0800

    13

--- bb/script.js
+++ bb/script.js
@@ -1,1 +1,16 @@
 
+setTimeout(() => {
+  document.getElementById("popup").classList.add("show");
+}, 3000);
+
+// On click, show envelopes
+document.getElementById("popup").addEventListener("click", () => {
+  document.getElementById("envelopes").style.display = "flex";
+});
+
+// Toggle envelopes back-to-back
+document.querySelectorAll(".envelope").forEach(env => {
+  env.addEventListener("click", () => {
+    env.classList.toggle("toggle");
+  });
+});
